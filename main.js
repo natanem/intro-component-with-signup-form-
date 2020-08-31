b0f52ad3aac8aba7c4b56ddb54114e0b16e23107
NATANEM T TADESSE <getnatiman@gmail.com>
2020-08-31 06:32:03 -0700

    fixed style issues

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ inputs = document.querySelectorAll('input');
 inputs.forEach((el)=>{
     el.addEventListener('blur', ()=>{
         if(!el.value){
-            el.parentElement.lastElementChild.style.opacity = 1;
+            el.parentElement.lastElementChild.style.display = "block";
         }else{
-            el.parentElement.lastElementChild.style.opacity = 0;
+            el.parentElement.lastElementChild.style.display = "none";
         }
       })
  })
